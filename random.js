ecd088591e6dc0f2a5942cb7e5e1f6475004e101
agungasyhari <agungasyhari@gmail.com>
2023-10-25 19:26:57 +0700

    test 1

---
+++ random.js
@@ -0,0 +1,15 @@
+
+
+const mobil = "Honda";
+
+const wilayah = mobil === "Honda" ? "bandung" : "jakarta";
+
+console.log("area :",wilayah);
+
+
+const input = "Agung";
+const panjangkalimat = input.length;
+
+for (let i =0; i < panjangkalimat; i++){
+    console.log(input[i]);
+}
